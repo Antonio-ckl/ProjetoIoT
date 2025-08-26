a0f5dc1ad5a7291e1dbd3f1ffff0e49ef2eeee9c
Antonio-ckl <antonio.cklebis@gmail.com>
2025-08-26 15:39:30 -0300

    List sensor

---
+++ app/Livewire/Sensores/SensorList.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Livewire\Sensores;
+
+use App\Models\Sensor;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class SensorList extends Component
+{
+
+    use WithPagination;
+     public $search = '';
+    public $perPage = 10;
+
+    protected $queryString = [
+        'search'=>['except'=>''],
+        'perPage'=>['except'=> 10]
+    ];
+    
+    public function render()
+    {
+
+          $sensor= Sensor::where( 'nome','like', "%{$this->search}%" )
+        ->orWhere('tipo', 'like', "%{$this->search}%")
+        ->orWhere('ambiente', 'like', "%{$this->search}%")
+        ->paginate($this->perPage);
+
+
+        return view('livewire.sensores.sensor-list');
+    }
+}
