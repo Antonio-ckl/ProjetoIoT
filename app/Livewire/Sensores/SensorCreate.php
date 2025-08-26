bdea03f9050b8db8c97af4559d17b020920eeee3
Antonio-ckl <antonio.cklebis@gmail.com>
2025-08-26 15:39:20 -0300

    Create sensor

--- app/Livewire/Sensores/SensorCreate.php
+++ app/Livewire/Sensores/SensorCreate.php
@@ -2,18 +2,55 @@
 
 namespace App\Livewire\Sensores;
 
+use App\Models\Ambiente;
+use App\Models\Sensor;
 use Livewire\Component;
 
 class SensorCreate extends Component
 {
-
+    public $ambiente_id;
     public $codigo;
     public $tipo;
-    
+    public $descricao;
+    public $status;
+
+
+    protected $rules = [
+        'codigo' => 'required|unique:sensor,codigo',
+        'tipo' => 'required'
+    ];
+
+    protected $messages = [
+        'codigo.required' => 'o codigo é obrigatorio',
+        'codigo.unique' => 'o codigo é unico',
+        'tipo.required' => 'o tipo é obrigatorio'
+    ];
+
+    public function store()
+    {
+        $this->validate();
+
+        $ambiente = Ambiente::create([
+            'ambiente_id' => $this->ambiente_id
+        ]);
+
+        Sensor::create([
+            'ambiente_id'=> $ambiente -> ambiente_id,
+            'codigo'=> $this->codigo,
+            'tipo'=>$this->descricao,
+            'status'=>$this->status
+        ]);
+
+        session()->flash('sucess ', 'Cadastro de Sensor realizado com sucesso');
+    }
+
 
 
     public function render()
     {
-        return view('livewire.sensores.sensor-create');
+
+        $ambientes = Ambiente::all();
+
+        return view('livewire.sensores.sensor-create', compact('ambientes'));
     }
 }
--- resources/views/livewire/sensores/sensor-create.blade.php
+++ resources/views/livewire/sensores/sensor-create.blade.php
@@ -1,3 +1,66 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="mt-5">
+   
+    <div class="d-flex align-items-center justify-content-center vh-100" style="background-color: #fff">
+        <div class="card shadow-sm p-4" style="width: 500px; background-color: #0700ca">
+            <h2 class="text-center mb-6" style="color:#ffffff; font-family:'Franklin Gothic Medium">Cadastro de
+                Ambiente</h2>
+            <div class="card-body">
+                <form wire:submit.prevent="store">
+                    <div class="mb-3">
+                        <label for="codigo" class="form-label" style="color: #fffeed">Codigo</label>
+                        <input type="string" class="form-control" id="codigo" name="codigo"
+                            placeholder="Insira seu codigo" wire:model.defer="codigo"
+                            style="border-radius: 100px; background-color:#fff">
+                        @error('nome')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                     <div class="mb-3">
+                        <label for="tipo" class="form-label" style="color: #fff">Tipo</label>
+                        <input type="string" class="form-control" id="tipo" name="tipo"
+                            placeholder="Insira seu tipo" wire:model.defer="tipo"
+                            style="border-radius: 100px; background-color:#fff">
+                        @error('nome')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="descricao" class="form-label" style="color: #fff">Descricao</label>
+                        <input type="text" class="form-control" id="Descricao" name="Descricao"
+                            placeholder="Insira sua Descricao" wire:model.defer="Descricao"
+                            style="border-radius: 100px; background-color:#fff">
+                        @error('Descricao')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="status" class="form-label" style="color: #fff; ">Status</label>
+                        <select class="form-select" id="status" name= "status" wire:model.defer="status"
+                            style="border-radius: 100px; background-color:#fff">
+                            <option hidden>Selecione seu status</option>
+                        <option value="0">Desativado</option>
+                        <option value="1">Ativado</option>
+                            
+                        </select>
+                    </div>
+                    
+                    <div class="mb-3">
+                        <label for="ambiente" class="form-label" style="color: #fff; ">Ambiente</label>
+                        <select class="form-select" id="ambiente" name= "ambiente" wire:model.defer="ambiente"
+                            style="border-radius: 100px; background-color:#fff">
+                            <option hidden>Selecione seu status</option>
+
+                             
+                            
+                        </select>
+                    </div>
+                    
+                    <div class="mb-2">
+                        <button type="submit" class="btn w-100"
+                            style="background-color: #000000; color:#fff; border-radius:100px">Cadastrar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
