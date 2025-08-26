9508af7dac02d69da68109c8400c886c0806ff37
Antonio-ckl <antonio.cklebis@gmail.com>
2025-08-26 15:39:38 -0300

    route sensor

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Livewire\Dashboard;
+use App\Livewire\Sensores\SensorCreate;
+use App\Livewire\Sensores\SensorList;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
+
+Route::get('/sensor/create', SensorCreate::class);
+
+Route::get('/sensor/list', SensorList::class);
