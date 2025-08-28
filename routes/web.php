68ecffbf7802e9fe89b1e391db1d01b2e95ed748
Antonio-ckl <antonio.cklebis@gmail.com>
2025-08-28 14:56:52 -0300

    rotas

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,15 @@
 
 use App\Livewire\Dashboard;
 use App\Livewire\Sensores\SensorCreate;
+use App\Livewire\Sensores\SensorEdit;
 use App\Livewire\Sensores\SensorList;
+use App\Models\Sensor;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
 
-Route::get('/sensor/create', SensorCreate::class);
+Route::get('/sensor/create', SensorCreate::class)->name('sensor.create');
 
-Route::get('/sensor/list', SensorList::class);
+Route::get('/sensor/list', SensorList::class)->name('sensor.list');
+
+Route::get('/sensor/edit', SensorEdit::class)->name('sensor.edit');
