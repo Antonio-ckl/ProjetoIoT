b0a1ad8cb21009e69d9353f3fd43ed0fea132443
Antonio-ckl <antonio.cklebis@gmail.com>
2025-09-16 15:37:37 -0300

    controller, web e api

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Livewire\Dashboard;
+use App\Livewire\Registro\RegistroIndex;
+use App\Models\Registro;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Dashboard::class);
+
+Route::get('/registro/index',RegistroIndex::class)->name('registro.index');
