4fbfbd6cb0d873f45c24157c51a097b7e02ecf2c
Antonio-ckl <antonio.cklebis@gmail.com>
2025-10-07 15:11:40 -0300

    Registro

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegistroRequest;
 use App\Models\Registro;
+use App\Models\Sensor;
 use Illuminate\Http\Request;
 
 class RegistroController extends Controller
 {
+    /*
     public function store(Request $request){
         $registro= Registro::create([
             'sensor_id'=>$request->sensor_id,
@@ -20,5 +23,25 @@ class RegistroController extends Controller
             'message'=>"Cadastrado com sucesso",
             'data'=>$registro
         ]);
-    }
+    }*/
+
+        public function store(RegistroRequest $request){
+            $sensor = Sensor::where('codigo', $request->cod_sensor)->first();
+
+            if(!$sensor){
+                return response()->json(['error'=> 'sensor não encontrado'],404);
+
+            }
+
+            $registro = Registro::create(['sensor_id'=> $sensor->id,
+            'valor'=> $request->valor,
+            'unidade'=> $request->unidade,
+            'data_hora'=> now()
+        ]);
+
+            return response()->json([
+                'success'=>'registro salvo com sucesso',
+                'data'=> $registro
+            ],201);
+        }
 }
---
+++ app/Http/Requests/RegistroRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException as ValidationException;
+
+class RegistroRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'cod_sensor'=>'required',
+            'valor'=>'required|numeric',
+            'unidade'=>'required'
+        ];
+
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        if($this-> expectsJson()){
+            throw new HttpResponseException(response()->json([
+                'success'=> false,
+                'message'=>'Erro de validação',
+                'errors'=>$validator->errors()
+            ],422));
+        }
+
+        throw new ValidationException($validator);
+    }
+
+    public function messages()
+    {
+        return  [
+            'cod_sensor.required'=>'o codigo do sensor é obrigatorio',
+            'valor.required'=>'o valor do sensor é obrigatorio',
+            'valor.numeric'=> 'o valor do sensor precisa ser numerico',
+            'unidade.required'=>'a unidade de medida é obrigatoria'
+
+        ];
+    }
+
+}
