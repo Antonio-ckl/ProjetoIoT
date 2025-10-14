5904860f0ee2d8d55abdb2c85c30b81d5fdb83ba
Antonio-ckl <antonio.cklebis@gmail.com>
2025-10-14 15:42:13 -0300

    Api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
     {
       $this->call([
         AmbienteSeeder::class,
-        SensorSeeder::class,
+        //SensorSeeder::class,
        // RegistroSeeder::class
       ]);
 
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\DispositivoController;
 use App\Http\Controllers\RegistroController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('registro',[RegistroController::class,'store'])->name('registro');
+
+Route::post('dispositivo',[DispositivoController::class, 'store'])->name('dispositivo');
 
 
 
