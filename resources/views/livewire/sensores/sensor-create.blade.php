35daaf66c1f2b400e67ced99299338b1bb9f64a4
Antonio-ckl <antonio.cklebis@gmail.com>
2025-10-14 15:41:49 -0300

    Sensor

--- resources/views/livewire/sensores/sensor-create.blade.php
+++ resources/views/livewire/sensores/sensor-create.blade.php
@@ -1,5 +1,10 @@
 <div class="mt-5">
-   
+   @if (session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="btn btn-close" data-bs-dismiss="alert" aria-label="close"></button>
+            </div>
+        @endif
    
     <div class="d-flex align-items-center justify-content-center vh-100" >
         
